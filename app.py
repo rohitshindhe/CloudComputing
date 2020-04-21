03b0c454d07bcd7ed27599fe4afb324e49cf1c11
rohitshindhe <51249107+rohitshindhe@users.noreply.github.com>
2020-04-21 08:14:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,113 +11,79 @@ from OpenSSL import SSL
 # session = cluster.connect()
 
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
-
+#app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-app.config['SECRET_KEY'] = 'thisisasecret'
+app.config['SECRET_KEY'] = 'this is a secret'
 
 db = SQLAlchemy(app)
 
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
     name = db.Column(db.String(50), nullable=False)
 
 
 def get_weather_data(city):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid=b835cfedcebb2963368761bcda01d49b'
     r = requests.get(url).json()
-
     return r
 
 
 @app.route('/')
 def index_get():
     cities = City.query.all()
-
     weather_data = []
 
     for city in cities:
         r = get_weather_data(city.name)
-
         print(r)
 
         weather = {
-
             'city': city.name,
-
             'temperature': r['main']['temp'],
-
             'description': r['weather'][0]['description'],
-
             'icon': r['weather'][0]['icon'],
-
         }
-
         weather_data.append(weather)
-
     return render_template('weather.html', weather_data=weather_data)
 
 
 @app.route('/', methods=['POST'])
 def index_post():
     err_msg = ''
-
     new_city = request.form.get('city')
-
     if new_city:
-
-        existing_city = City.query.filter_by(name=new_city).first()
-
+        existing_city = City.query.filter_by(name=upper(new_city)).first()
         if not existing_city:
-
             new_city_data = get_weather_data(new_city)
-
             if new_city_data['cod'] == 200:
-
-                new_city_obj = City(name=new_city)
-
+                new_city_obj = City(name=upper(new_city))
                 db.session.add(new_city_obj)
-
                 db.session.commit()
 
             else:
-
                 err_msg = 'City does not exist in the world!'
 
         else:
-
             err_msg = 'City already exists in the database!'
 
     if err_msg:
-
         flash(err_msg, 'error')
 
     else:
-
         flash('City added successfully!')
-
     return redirect(url_for('index_get'))
 
 
 @app.route('/delete/<name>')
 def delete_city(name):
     city = City.query.filter_by(name=name).first()
-
     db.session.delete(city)
-
     db.session.commit()
-
     flash(f'Successfully deleted {city.name}', 'success')
-
     return redirect(url_for('index_get'))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
